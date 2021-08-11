3f7f66041cafeb7ad422e01649c93e75931b800c
nhanntce <nguyennhan3639@gmail.com>
2021-08-11 16:32:07 +0700

    Plagiarism unit test NhanNT

--- src/utility/Plagiarismer.java
+++ src/utility/Plagiarismer.java
@@ -20,8 +20,9 @@ public class Plagiarismer {
 
     public static ArrayList<Plagiarism> plagiarism(String fileCheck, ArrayList<String> listCheck, String type, frmJudge parent) throws IOException {
         ArrayList<Plagiarism> res = new ArrayList<>();
-        if("py".equals(type))
+        if ("py".equals(type)) {
             return res;
+        }
         Runtime r = Runtime.getRuntime();
         Process process = null;
         BufferedReader br1 = null;
@@ -33,62 +34,65 @@ public class Plagiarismer {
         for (String s : listCheck) {
             cmdPercentage += " \"" + s + "\"";
         }
-        switch (parent.ostype) {
-            case Windows:
-                if ("c".equals(type)) {
+
+        if (null != type) {
+            switch (type) {
+                case "c":
                     process = r.exec("sim_c.exe -p " + cmdPercentage);
-                } else if ("cpp".equals(type)) {
+                    break;
+                case "cpp":
                     process = r.exec("\"sim_c++.exe\" -p " + cmdPercentage);
-                } else if ("java".equals(type)) {
+                    break;
+                case "java":
                     process = r.exec("\"sim_java.exe\" -p " + cmdPercentage);
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            return res;
+        }
+        br1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        ArrayList<String> resultPercentage = new ArrayList<>();
+        String s = "";
+        while ((s = br1.readLine()) != null) {
+            resultPercentage.add(s);
+        }
+        String result = resultPercentage.get(resultPercentage.size() - 1).trim();
+        if (!result.equals("")) {
+            try {
+                plagiarismPath = result.substring(result.indexOf(" % of ") + 6, result.length() - 9);
+                percentage = Integer.parseInt(result.substring(result.indexOf(" consists for ") + 14, result.indexOf(" % of ")));
+                res.add(new Plagiarism(percentage, plagiarismPath));
+            } catch (Exception e) {
+
+            }
+            if (percentage > 0) {
+                cmdSimilar += "\"" + plagiarismPath + "\"";
+                if ("c".equals(type)) {
+                    process = r.exec("sim_c.exe -n " + cmdSimilar);
+                } else if ("cpp".equals(type)) {
+                    process = r.exec("\"sim_c++.exe\" -n " + cmdSimilar);
+                } else if ("java".equals(type)) {
+                    process = r.exec("\"sim_java.exe\" -n " + cmdSimilar);
                 }
                 br1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                ArrayList<String> resultPercentage = new ArrayList<>();
-                String s = "";
-                while ((s = br1.readLine()) != null) {
-                    resultPercentage.add(s);
+                ArrayList<String> resultSimilar = new ArrayList<>();
+                String sSemilar = "";
+                while ((sSemilar = br1.readLine()) != null) {
+                    resultSimilar.add(sSemilar);
                 }
-                String result = resultPercentage.get(resultPercentage.size() - 1).trim();
-                if (!result.equals("")) {
-                    try {
-                        plagiarismPath = result.substring(result.indexOf(" % of ") + 6, result.length() - 9);
-                        percentage = Integer.parseInt(result.substring(result.indexOf(" consists for ") + 14, result.indexOf(" % of ")));
-                        res.add(new Plagiarism(percentage, plagiarismPath));
-                    } catch (Exception e) {
-
-                    }
-                    if (percentage > 0) {
-                        cmdSimilar += "\"" + plagiarismPath + "\"";
-                        if ("c".equals(type)) {
-                            process = r.exec("sim_c.exe -n " + cmdSimilar);
-                        } else if ("cpp".equals(type)) {
-                            process = r.exec("\"sim_c++.exe\" -n " + cmdSimilar);
-                        } else if ("java".equals(type)) {
-                            process = r.exec("\"sim_java.exe\" -n " + cmdSimilar);
-                        }
-                        br1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                        ArrayList<String> resultSimilar = new ArrayList<>();
-                        String sSemilar = "";
-                        while ((sSemilar = br1.readLine()) != null) {
-                            resultSimilar.add(sSemilar);
-                        }
-                        for (int i = 5; i < resultSimilar.size(); i += 2) {
-                            String fullStr = resultSimilar.get(i);
-                            String lineOriginal = fullStr.substring(fullStr.indexOf(": line ") + 7,
-                                    fullStr.indexOf("|")).trim().replace("-", " ");
-                            String lineSimilar = fullStr.substring(fullStr.lastIndexOf(": line ") + 7,
-                                    fullStr.lastIndexOf("[")).trim().replace("-", " ");
-                            res.add(new Plagiarism(lineOriginal, lineSimilar));
-                        }
-                    }
+                for (int i = 5; i < resultSimilar.size(); i += 2) {
+                    String fullStr = resultSimilar.get(i);
+                    String lineOriginal = fullStr.substring(fullStr.indexOf(": line ") + 7,
+                            fullStr.indexOf("|")).trim().replace("-", " ");
+                    String lineSimilar = fullStr.substring(fullStr.lastIndexOf(": line ") + 7,
+                            fullStr.lastIndexOf("[")).trim().replace("-", " ");
+                    res.add(new Plagiarism(lineOriginal, lineSimilar));
                 }
-                break;
-            case MacOS:
-                break;
-            case Linux:
-            case Other:
-                break;
+            }
         }
+
         return res;
     }
 }
--- test/utility/PlagiarismerTest.java
+++ test/utility/PlagiarismerTest.java
@@ -1,5 +1,9 @@
 package utility;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import judge.frmJudge;
+import obj.Plagiarism;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
@@ -7,13 +11,40 @@ import org.junit.Test;
 
 public class PlagiarismerTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    frmJudge parent = new frmJudge();
 
-	@Test
-	public void testPlagiarism() {
-		fail("Not yet implemented");
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testPlagiarism() throws IOException {
+        ArrayList<String> fileChecks = new ArrayList<>();
+        ArrayList<ArrayList<String>> listChecks = new ArrayList<>();
+        ArrayList<String> types = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<String> list3 = new ArrayList<>();
+        fileChecks.add("source code for unit test/plagiarism/C1.c");
+        fileChecks.add("source code for unit test/plagiarism/Cpp1.cpp");
+        fileChecks.add("source code for unit test/plagiarism/Java1.java");
+        list1.add("source code for unit test/plagiarism/C2.c");
+        list2.add("source code for unit test/plagiarism/Cpp2.cpp");
+        list3.add("source code for unit test/plagiarism/Java2.java");
+        listChecks.add(list1);
+        listChecks.add(list2);
+        listChecks.add(list3);
+        types.add("c");
+        types.add("cpp");
+        types.add("java");
+
+        for (int i = 0, len = fileChecks.size(); i < len; i++) {
+            ArrayList<Plagiarism> plagiarism = Plagiarismer.plagiarism(fileChecks.get(i), listChecks.get(i), types.get(i), parent);
+        }
+
+    }
 
 }
