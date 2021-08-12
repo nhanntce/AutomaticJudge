f4af3aba57d33045051efd7c6dbb865b5df0a418
nhanntce <nguyennhan3639@gmail.com>
2021-08-12 08:56:36 +0700

    Plagiarism unit test NhanNT

--- test/utility/PlagiarismerTestTest.java
+++ test/utility/PlagiarismerTestTest.java
@@ -15,23 +15,26 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author PC
+ * @author NhanNT
  */
-public class PlagiarismerTest {
+public class PlagiarismerTestTest {
+
     frmJudge parent = new frmJudge();
-    public PlagiarismerTest() {
+
+    public PlagiarismerTestTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
 
     /**
      * Test of plagiarism method, of class Plagiarismer.
+     * This function has 4 test cases
      */
     @Test
     public void testPlagiarism() throws Exception {
@@ -42,39 +45,49 @@ public class PlagiarismerTest {
         ArrayList<String> list2 = new ArrayList<>();
         ArrayList<String> list3 = new ArrayList<>();
         ArrayList<String> list4 = new ArrayList<>();
-        
-        fileChecks.add("source code for unit test/plagiarism/C1.c");
-        fileChecks.add("source code for unit test/plagiarism/Cpp1.cpp");
-        fileChecks.add("source code for unit test/plagiarism/Java1.java");
-        fileChecks.add("source code for unit test/plagiarism/py1.py");
-        
+        //first parameters
+        fileChecks.add("source code for unit test/plagiarism/C1");
+        fileChecks.add("source code for unit test/plagiarism/Cpp1");
+        fileChecks.add("source code for unit test/plagiarism/Java1");
+        fileChecks.add("source code for unit test/plagiarism/py1");
+        //second parameter
         list1.add("source code for unit test/plagiarism/C2.c");
         list2.add("source code for unit test/plagiarism/Cpp2.cpp");
         list3.add("source code for unit test/plagiarism/Java2.java");
         list4.add("source code for unit test/plagiarism/py2.py");
-        
         listChecks.add(list1);
         listChecks.add(list2);
         listChecks.add(list3);
         listChecks.add(list4);
-        
+        //third parameter
         types.add("c");
         types.add("cpp");
         types.add("java");
         types.add("py");
-        
         // expected
-        ArrayList<Integer> expected = new ArrayList<>();
-        expected.add(41);
-        expected.add(100);
-        expected.add(0);
-        
+        ArrayList<ArrayList<Plagiarism>> expected = new ArrayList<>();
+        ArrayList<Plagiarism> exp1 = new ArrayList<>();
+        ArrayList<Plagiarism> exp2 = new ArrayList<>();
+        exp1.add(new Plagiarism(41, "source code for unit test/plagiarism/C2.c"));
+        exp1.add(new Plagiarism("35 37", "67 69"));
+        exp1.add(new Plagiarism("21 27", "53 59"));
+        exp1.add(new Plagiarism("4 9", "6 11"));
+        expected.add(exp1);
+        exp2.add(new Plagiarism(100, "source code for unit test/plagiarism/Cpp2.cpp"));
+        exp2.add(new Plagiarism("3 51", "3 51"));
+        expected.add(exp2);
+        //assert
         for (int i = 0, len = fileChecks.size(); i < len; i++) {
             ArrayList<Plagiarism> plagiarism = Plagiarismer.plagiarism(fileChecks.get(i), listChecks.get(i),
                     types.get(i), parent);
-            if(plagiarism.size() > 0)
-                assertEquals(expected.get(i), plagiarism.get(0));
-            else {
+            if (plagiarism.size() > 0) {
+                assertEquals(plagiarism.get(0).getPercentageOfPlagiarism(), expected.get(i).get(0).getPercentageOfPlagiarism());
+                assertEquals(plagiarism.get(0).getNameOfFileHasPlagiarism(), expected.get(i).get(0).getNameOfFileHasPlagiarism());
+                for (int j = 1, len1 = plagiarism.size(); j < len1; j++) {
+                    assertEquals(plagiarism.get(j).getCompareWith(), expected.get(i).get(j).getCompareWith());
+                    assertEquals(plagiarism.get(j).getOrigianal(), expected.get(i).get(j).getOrigianal());
+                }
+            } else {
                 assertTrue(true);
             }
         }
